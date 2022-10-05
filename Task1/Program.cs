a50b8b57450c42b490ca95b34a491238646e9696
 mac <kksanaa@mail.ru>
2022-10-05 19:14:45 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+void AnswerToTask(int number)
+{
+    string numericAsString = number.ToString();
+    int result = int.Parse(numericAsString[1].ToString());
+    Console.WriteLine($"Result numeric: {result}");
+}
+AnswerToTask(567);
