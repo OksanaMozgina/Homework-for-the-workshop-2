13d6647572548b32f2b0ecc3e302e14ddde8d1c8
 mac <kksanaa@mail.ru>
2022-10-06 20:26:41 +0300

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+
+void CheckingTheDayOfTheWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine($"{dayNumber} -> да");
+    }
+    else 
+    {
+    Console.WriteLine($"{dayNumber} -> нет");
+    }
+}
+
+CheckingTheDayOfTheWeek(5);
